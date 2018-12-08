ee512d1034b539a6eb6dd24626c97bdf83e119b1
Christopher Lee <xchrislee@gmail.com>
2018-12-08 10:25:19 -0800

    Modify server code to test api routes

--- server.js
+++ server.js
@@ -1,16 +1,31 @@
 var express = require('express');
 var app = express();
+const bodyParser = require('body-parser');
 const mechMarket = require('./mechMarket.js');
+
+const port = process.env.PORT || 3001;
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get('/', function(req, res) {
   res.send('Hello World!');
 });
 
-app.get('/listings', function(req, res) {
-  mechMarket.getAuthKey();
-  res.send('listings');
+app.get('/api/hello', (req, res) => {
+  res.send({ express: 'Hello From Express' });
 });
 
-app.listen(3001, function() {
+app.get('/api/listings', function(req, res, next) {
+  mechMarket.getAuthKey().then(data => {
+    const authToken = data;
+    mechMarket.getPosts(authToken).then(data => {
+      console.log('got here');
+      res.send(data);
+      next();
+    });
+  });
+});
+
+app.listen(port, function() {
   console.log('Example app listening on port 3001');
 });
